4f03e8a66b604b2d719877efe2c574e81b927e36
kevin5097 <createmakeapp@gmail.com>
2025-12-23 12:04:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const SUPABASE_URL = "https://inodjrrjvpmmeoemasgu.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_apoipSCp-C_ddztbyfFctg_tajGzTjH";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
 async function loadRoutines() {
   const { data, error } = await supabase
     .from("routines")
