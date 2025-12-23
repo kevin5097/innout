a13f6d37e1c37b0110382754b7fc19ff7834974c
kevin5097 <createmakeapp@gmail.com>
2025-12-23 12:01:38 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://inodjrrjvpmmeoemasgu.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_apoipSCp-C_ddztbyfFctg_tajGzTjH";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
